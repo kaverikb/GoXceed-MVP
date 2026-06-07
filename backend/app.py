65bb3fb76c115c2c76c06bd7b99d37556f61a2e5
kaverikb <kaverikb18@gmail.com>
2026-06-07 23:01:57 +0530

    Add Vercel URL to CORS origins

--- backend/app.py
+++ backend/app.py
@@ -15,6 +15,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173"
+        "https://go-xceed-mvp.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
